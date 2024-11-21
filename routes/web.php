9afac31a42c2156b5f698f8b371409e4127643d2
Marcelo Pinheiro <pinheiro84@gmail.com>
2024-11-21 20:27:37 -0300

    Acrescentando rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello World!";
+});
+
+Route::get('/about', function () {
+    echo 'About us';
 });
