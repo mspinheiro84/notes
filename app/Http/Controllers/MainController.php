e60cc318ce59feb76d8f5ae33547ca4f95d25828
Marcelo Pinheiro <pinheiro84@gmail.com>
2024-11-21 21:01:21 -0300

    Limpando as pastas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,19 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($value) 
-    {
-        // return view('main', ['value' => $value]);
-        return view('main')->with('value', $value);
-    }
 
-    public function page2 ($value)
-    {
-        return view('page2')->with('value', $value);
-    }
-
-    public function page3 ($value)
-    {
-        return view('page3')->with('value', $value);
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo "Hello World!";
-});
-
-Route::get('/about', function () {
-    echo 'About us';
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
