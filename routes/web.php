bfd66e1d24ceb76aaf5d81ad37dec7def3b42443
Marcelo Pinheiro <pinheiro84@gmail.com>
2024-11-24 18:00:05 -0300

    Criação da rota logmSubmit

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 //auth routes
 
 Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
