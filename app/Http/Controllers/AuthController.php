f8c718e10a2b4d4aeb1f5b79a278c2bf7e1b4596
Marcelo Pinheiro <pinheiro84@gmail.com>
2024-11-24 18:32:14 -0300

    Validação dos campos login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,29 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo "<br>";
-        echo $request->input('text_password');
+        //form validation
+
+        $request->validate(
+            //rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16',
+            ],
+            //error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username deve ser um e-mail válido',
+                'text_password.required' => 'O password é obrigatóio',
+                'text_password.min' => 'O password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'O password deve ter no máximo :max caracteres',
+            ]
+        );
+
+        //get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo "Ok!";
     }
 
     public function logout()
