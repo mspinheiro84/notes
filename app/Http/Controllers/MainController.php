734bfa66d220ca0a6f718e733b30747ba4e9264d
Marcelo Pinheiro <pinheiro84@gmail.com>
2024-11-25 18:58:51 -0300

    criando serviço para decrypt id

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,13 +11,29 @@ class MainController extends Controller
     public function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
 
         // show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
     {
         echo "I'm creating a new note!";
+    }
+
+    public function editNote($id)
+    {
+        $id = Operations::decryptId($id);
+        
+        echo "I'm editing note with id = $id";
+    }
+
+    public function deleteNote($id)
+    {
+        $id = Operations::decryptId($id);
+        
+        echo "I'm deleting note with id = $id";
     }
 }
